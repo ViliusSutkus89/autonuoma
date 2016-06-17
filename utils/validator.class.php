ef98cfb06931a150bb04b63635bfd7a9d0c8eed7
Vilius Sutkus <ViliusSutkus89@gmail.com>
2016-06-17 13:10:07 +0000

    Escape HTML entities. If html chars are left as is, XSS attack could be performed on the site

--- utils/validator.class.php
+++ utils/validator.class.php
@@ -180,12 +180,47 @@ class validator
     	return ($filter === false) ? false : filter_var($var, $filter) !== false ? true : false;
     }
 
-    /*
-     * We're using PDO, no need for any database input sanitization
-     */
 	function preparePostFieldsForSQL() {
-    return $this->fields;
+		$data = array();
+
+		foreach($this->fields as $key=>$val) {
+			$tmp = null;
+			if(!is_array($val)) {
+				$tmp = $this->html_escape($val);
+			} else {
+				foreach($val as $key2 => $val2) {
+					$tmp[] = $this->html_escape($val2);
+				}
+			}
+			
+			if(!in_array($key, $this->mandatories) && ($tmp == '' || $tmp == array())) {
+				$data[$key] = '';
+			} else {
+				if(!is_array($tmp)) {
+					$data[$key] = $tmp;
+				} else {
+					$data[$key] = $tmp;
+				}
+				
+			}
+		}
+
+    return $data;
 	}
+
+    /*
+     * Escape HTML entities, to prevent XSS
+     */
+  function html_escape($input) {
+    $data = str_replace('&', '&amp;', $input);
+    $data = str_replace(
+      array('<', '>', '"', "'", "/"),
+      array('&lt;', '&gt;', '&quot;', '&#x27;', '&#x2f;'),
+      $data
+    );
+
+    return $data;
+  }
 	
 }
 
