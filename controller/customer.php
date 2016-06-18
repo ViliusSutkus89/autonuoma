f11f7e5a6869447a5dbe203022c08624e974a49a
Vilius Sutkus <ViliusSutkus89@gmail.com>
2016-06-18 00:08:58 +0000

    Convert customer controller to object

--- controller/customer.php
+++ controller/customer.php
@@ -3,116 +3,140 @@ require_once 'utils/paging.class.php';
 require_once 'utils/validator.class.php';
 require_once 'model/customers.class.php';
 
-$customersObj = new customers();
+class customerController {
 
-if (!empty($id) || $action == 'new') {
-  $formErrors = null;
-  $fields = array();
-  
-  // nustatome privalomus formos laukus
-  $required = array('asmens_kodas', 'vardas', 'pavarde', 'gimimo_data', 'telefonas');
+  public static $defaultAction = "index";
 
-  // maksimalūs leidžiami laukų ilgiai
-  $maxLengths = array (
+	// nustatome privalomus laukus
+  private $required = array('asmens_kodas', 'vardas', 'pavarde', 'gimimo_data', 'telefonas');
+
+	// maksimalūs leidžiami laukų ilgiai
+  private $maxLengths = array (
     'asmens_kodas' => 11,
     'vardas' => 20,
     'pavarde' => 20
   );
 
-	// vartotojas paspaudė išsaugojimo mygtuką
-	if(!empty($_POST['submit'])) {
-
-		// nustatome laukų validatorių tipus
-		$validations = array (
+  // nustatome laukų validatorių tipus
+  private $validations = array (
 			'asmens_kodas' => 'positivenumber',
 			'vardas' => 'alfanum',
 			'pavarde' => 'alfanum',
 			'gimimo_data' => 'date',
 			'telefonas' => 'phone',
 			'epastas' => 'email'
-		);
-		
-		// sukuriame laukų validatoriaus objektą
-		$validator = new validator($validations, $required, $maxLengths);
+  );
+
+  public function indexAction() {
+    // sukuriame customerių klasės objektą
+    $customersObj = new customers();
+
+    // suskaičiuojame bendrą įrašų kiekį
+    $elementCount = $customersObj->getCustomersListCount();
+
+    // sukuriame puslapiavimo klasės objektą
+    $paging = new paging(NUMBER_OF_ROWS_IN_PAGE);
+
+    // suformuojame sąrašo puslapius
+    $paging->process($elementCount, routing::getPageId());
+
+    // išrenkame nurodyto puslapio markes
+    $data = $customersObj->getCustomersList($paging->size, $paging->first);
+
+    $template = template::getInstance();
+
+    $template->assign('data', $data);
+    $template->assign('pagingData', $paging->data);
+
+    if(!empty($_GET['remove_error']))
+      $template->assign('remove_error', true);
+
+    $template->setView("customer_list");
+  }
+
+  public function editAction() {
+    if (!empty($_POST['submit']))
+      $this->insertUpdateAction();
+    else
+      $this->showAction();
+  }
+
+  private function showAction() {
+    $id = routing::getId();
+
+    $customersObj = new customers();
+
+    $fields = array();
+		// tikriname, ar nurodytas elemento id. Jeigu taip, išrenkame elemento duomenis ir jais užpildome formos laukus.
+    if ($id) {
+      $fields = $customersObj->getCustomer($id);
+			$fields['editing'] = 1;
+    }
+
+    $template = template::getInstance();
+
+    $template->assign('fields', $fields);
+    $template->assign('required', $this->required);
+    $template->assign('maxLengths', $this->maxLengths);
+
+    $template->setView("customer_edit");
+  }
+
+  private function insertUpdateAction() {
+		// sukuriame validatoriaus objektą
+    $validator = new validator($this->validations, $this->required, $this->maxLengths);
 
 		// laukai įvesti be klaidų
 		if($validator->validate($_POST)) {
+      $customersObj = new customers();
+
 			// suformuojame laukų reikšmių masyvą SQL užklausai
 			$data = $validator->preparePostFieldsForSQL();
-
 			if(isset($data['editing'])) {
-				// redaguojame klientą
+				// atnaujiname duomenis
 				$customersObj->updateCustomer($data);
 			} else {
 				// įrašome naują klientą
 				$customersObj->insertCustomer($data);
 			}
 
-			// nukreipiame vartotoją į klientų puslapį
-			header("Location: index.php?module={$module}");
-      $template->disableRendering();
-		}
-		else {
+			// nukreipiame į customerių puslapį
+      routing::redirect(routing::getModule(), 'index');
+		} else {
+      $this->showAction();
+
+      $template = template::getInstance();
+
+      // Overwrite fields array with submitted $_POST values
+      $template->assign('fields', $_POST);
+
 			// gauname klaidų pranešimą
 			$formErrors = $validator->getErrorHTML();
-			// laukų reikšmių kintamajam priskiriame įvestų laukų reikšmes
-			$fields = $_POST;
+      $template->assign('formErrors', $formErrors);
 		}
-	} else {
-		// tikriname, ar nurodytas elemento id. Jeigu taip, išrenkame elemento duomenis ir jais užpildome formos laukus.
-		if(!empty($id)) {
-			// išrenkame klientą
-			$fields = $customersObj->getCustomer($id);
-			$fields['editing'] = 1;
-		}
-	}
-  
-  $template->assign('fields', $fields);
-  $template->assign('required', $required);
-  $template->assign('maxLengths', $maxLengths);
-  
-  $template->assign('formErrors', $formErrors);
-  
-  $template->setView("customer_edit");
-}
-
-else if(!empty($removeId)) {
-  // patikriname, ar klientas neturi sudarytų sutarčių
-  $count = $customersObj->getContractCountOfCustomer($removeId);
-  
-  $removeErrorParameter = '';
-  if($count == 0) {
-  	// šaliname klientą
-  	$customersObj->deleteCustomer($removeId);
-  } else {
-  	// nepašalinome, nes klientas sudaręs bent vieną sutartį, rodome klaidos pranešimą
-  	$removeErrorParameter = '&remove_error=1';
   }
-  
-  // nukreipiame į klientų puslapį
-  header("Location: index.php?module={$module}{$removeErrorParameter}");
-  $template->disableRendering();
-}
 
-// View list
-else {
-  // suskaičiuojame bendrą įrašų kiekį
-	$elementCount = $customersObj->getCustomersListCount();
+  public function removeAction() {
+    $id = routing::getId();
 
-  // sukuriame puslapiavimo klasės objektą
-  $paging = new paging(NUMBER_OF_ROWS_IN_PAGE);
+    // patikriname, ar klientas neturi sudarytų sutarčių
+    $customersObj = new customers();
+    $count = $customersObj->getContractCountOfCustomer($id);
 
-  // suformuojame sąrašo puslapius
-  $paging->process($elementCount, $pageId);
+    $removeErrorParameter = '';
+    if($count == 0) {
+      // šaliname klientą
+      $customersObj->deleteCustomer($id);
+    } else {
+      // nepašalinome, nes klientas sudaręs bent vieną sutartį, rodome klaidos pranešimą
+      // rodome klaidos pranešimą
+      $removeErrorParameter = 'remove_error=1';
+    }
 
-  // išrenkame nurodyto puslapio klientus
-  $data = $customersObj->getCustomersList($paging->size, $paging->first);
-  $template->assign('data', $data);
-  $template->assign('pagingData', $paging->data);
+    // nukreipiame į markių puslapį
+    routing::redirect(routing::getModule(), 'index',
+      $removeErrorParameter);
+  }
 
-  $template->setView("customer_list");
-
-  if(isset($_GET['remove_error']))
-    $template->assign('remove_error', true);
-}
+};
 
